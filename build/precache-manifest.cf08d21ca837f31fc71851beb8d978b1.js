60ce8a9401e58fcc9cbdcf405fceaafb67515bbf
Talal Abbas <talalabbas84@gmail.com>
2023-02-06 17:41:17 +0500

    build folder

---
+++ build/precache-manifest.cf08d21ca837f31fc71851beb8d978b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5707b0b0415aea8d4b116f2f207dc310",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fee12f7f7ef77b85f5e4",
+    "url": "/static/css/main.a3e5739a.chunk.css"
+  },
+  {
+    "revision": "e34d5c051a4e13a2a7cf",
+    "url": "/static/js/2.9512d020.chunk.js"
+  },
+  {
+    "revision": "4ad47703d6b1564c0b67885d6a2db9f1",
+    "url": "/static/js/2.9512d020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee12f7f7ef77b85f5e4",
+    "url": "/static/js/main.ef5804cd.chunk.js"
+  },
+  {
+    "revision": "91ed4673c2ef0617256b",
+    "url": "/static/js/runtime-main.b451b96f.js"
+  }
+]);
